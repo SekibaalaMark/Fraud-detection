d84769ad9163e326f76ca3adec9e413470ab7a49
sekibaalamark <markmajje256@gmail.com>
2026-03-19 17:04:41 +0300

    imported the necessary dependencies

---
+++ fraud-detection.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
