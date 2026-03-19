324eeb68de0b5468d150bd927dc78ebcab358c0b
sekibaalamark <markmajje256@gmail.com>
2026-03-19 17:06:27 +0300

    loaded the dataset as DataFrame

--- fraud-detection.py
+++ fraud-detection.py
@@ -1,3 +1,4 @@
+#imported libraries for data manipulation, visualization, and machine learning
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -5,3 +6,6 @@ import matplotlib.pyplot as plt
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+
+# Load the dataset
+df = pd.read_csv('creditcard.csv')
