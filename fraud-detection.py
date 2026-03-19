c91460f5766859d6f94670e1703ab1180807d9ee
sekibaalamark <markmajje256@gmail.com>
2026-03-19 17:14:44 +0300

    scaling the features using standard scaler

--- fraud-detection.py
+++ fraud-detection.py
@@ -27,3 +27,7 @@ y = df['Class'] #Target variable (fraud or non-fraud)
 
 X_train , X_test , y_train , y_test = train_test_split(X,y,test_size=0.3,random_state=42,stratify=y)# Split the data into training and testing sets (70% train, 30% test) with stratification to maintain class distribution
 
+# Scale the features using StandardScaler
+scaler = StandardScaler() # Initialize the StandardScaler to standardize the features (mean=0, variance=1)
+X_train_scaled = scaler.fit_transform(X_train) # Fit the scaler on the training data and transform it to scale the features
+X_test_scaled = scaler.transform(X_test) # Transform the test data using the same scaler (do not fit again to avoid data leakage)
